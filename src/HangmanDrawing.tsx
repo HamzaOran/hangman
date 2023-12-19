faacaeb1e94ec21c6bb692f804772a8a2d05bfb3
HamzaOran <60844909+HamzaOran@users.noreply.github.com>
2023-12-19 16:28:49 +0300

    Update HangmanDrawing.tsx

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -115,13 +115,13 @@ export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
       />
       <div
         style={{
-          height: '300px',
+          height: '270px',
           width: '10px',
           background: 'black',
           marginLeft: '120px',
         }}
       />
-      <div style={{ height: '10px', width: '250px', background: 'black' }} />
+      <div style={{ height: '10px', width: '200px', background: 'black' }} />
     </div>
   );
 }
